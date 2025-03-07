32849cc2eb381542bfd0b6bf4ab56c0dd0d39479
Abdul Ghaffar <agscontact777@gmail.com>
2025-03-07 15:08:30 +0500

    Created list class

---
+++ list.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "error.cpp"
+
+using namespace std;
+
+template <typename T>
+class List {
+private:
+    T* data;        // Dynamic array
+    int capacity;   // Current allocated size
+    int size;       // Number of elements
+
+    void resize();  // Function to resize list
+
+public:
+    List(int cap = 10);  // Constructor
+    ~List();             // Destructor
+
+    void push(T value);  // Add element
+    void pop();          // Remove last element
+
+    T& operator[](int index);       // Overload [] operator for access
+    const T& operator[](int index) const; // Read-only version
+
+    int getSize() const; // Get current size
+};
+
+// Constructor
+template <typename T>
+List<T>::List(int cap) : capacity(cap), size(0) {
+    data = new T[capacity];
+}
+
+// Destructor
+template <typename T>
+List<T>::~List() {
+    delete[] data;
+}
+
+// Resize function (doubles capacity)
+template <typename T>
+void List<T>::resize() {
+    capacity *= 2;
+    T* newData = new T[capacity];
+    for (int i = 0; i < size; i++) {
+        newData[i] = data[i];
+    }
+    delete[] data;
+    data = newData;
+}
+
+// Add element to the list
+template <typename T>
+void List<T>::push(T value) {
+    if (size == capacity) resize();
+    data[size++] = value;
+}
+
+// Remove last element
+template <typename T>
+void List<T>::pop() {
+    if (size > 0) size--;
+}
+
+// Overload [] for accessing elements
+template <typename T>
+T& List<T>::operator[](int index) {
+    if (index < 0 || index >= size) {
+        throw Error("Index out of range");
+    }
+    return data[index];
+}
+
+// Overloaded [] for read-only access (const version)
+template <typename T>
+const T& List<T>::operator[](int index) const {
+    if (index < 0 || index >= size) {
+        throw Error("Index out of range");
+    }
+    return data[index];
+}
+
+// Get the current number of elements
+template <typename T>
+int List<T>::getSize() const {
+    return size;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "error.cpp"
 using namespace std;
 
@@ -6,11 +7,34 @@ int main(int argc, char *argv[])
 {
     try
     {
-        // Checking if the program was run with arguments
+        // Checking if the program was run with file name as argument
         if (argc == 1)
         {
             throw Error("No source file provided");
         }
+
+        // checking if the file has extension .gr
+        string filename = argv[1];
+        if (filename.substr(filename.find_last_of(".") + 1) != "gr")
+        {
+            throw Error("Invalid file type");
+        }
+        
+        // Checking if the file exists
+        ifstream file(filename);
+        if (!file)
+        {
+            throw Error("File not found: " + filename);
+        }
+
+        // Reading the contents of the file
+        string line;
+        while (getline(file, line))
+        {
+            cout << line << endl;
+        }
+
+        file.close();
     }
     catch (Error e)
     {
