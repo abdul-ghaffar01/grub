fa05a6cd0436b3af6e4a3bae09a38efaa3d85f99
Abdul Ghaffar <agscontact777@gmail.com>
2025-03-09 21:22:11 +0500

    Created extension for syntax

---
+++ grub-ext/README.md
@@ -0,0 +1,14 @@
+# GRUB Language Extension
+
+This is a VS Code extension for the **GRUB programming language**.
+
+## Features
+- Syntax highlighting for `.gr` files.
+- Support for raw C++ blocks inside GRUB code.
+- Custom keywords for GRUB language.
+
+## Installation
+1. Download the `.vsix` file after packaging.
+2. Install using:  
+   ```sh
+   code --install-extension grub-ext.vsix
--- output.cpp
+++ output.cpp
@@ -1,14 +1,21 @@
+    #include <cmath>
 
 #include <iostream>
 using namespace std;
 int main() {
-    int a = 10 ;
-    int b = 12 ;
-    if (a > b ) 
-        cout << "a is greater than b" << "\n";
-    else if (a < b ) 
-        cout << "b is greater than a" << "\n";
-    else 
-        cout << "a and b are equal" << "\n";
+    int i = 0 ;
+    while (i < 5 ) {
+        cout << "value of i is"  << " " << i << "\n";
+    i = i+1 ;
+}
+    cout << "Execution of cpp code starts here" << endl;
+    // this is a comment of cpp
+    for(int i = 0; i<5; i++){
+        for(int j = 0; j<=i; j++){
+            cout << "* ";
+        }cout << endl;
+    }
+    cout << "The square root of 16 is " << sqrt(16) << endl;
+    cout << "Execution of cpp code ends here" << endl;
     return 0;
 }
